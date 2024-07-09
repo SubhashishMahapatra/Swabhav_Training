340db7f233a8e40878162ee2fac8e62d7f810755
Subhashish Mahapatra <92688857+SubhashishMahapatra@users.noreply.github.com>
2024-07-09 20:36:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("The Factorial of a given number is: "+factorial);
+        }
+    }
+}
