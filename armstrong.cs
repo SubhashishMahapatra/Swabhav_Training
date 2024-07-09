37326f47f0a5d547c5b43bf21464c358d87fcce0
Subhashish Mahapatra <92688857+SubhashishMahapatra@users.noreply.github.com>
2024-07-09 20:44:43 +0530

    Updated armstrong.cs

--- armstrong.cs
+++ armstrong.cs
@@ -1,43 +1,50 @@
 using System;
 
-public class Armstrong{
-static void Main(string [] args){
-int num;
-int digit;
-int temp;
-int result = 0;
-int power;
+public class Armstrong
+{
+    static void Main(string[] args)
+    {
+        int num;
+        int digit;
+        int temp;
+        int result = 0;
+        int power;
 
-Console.WriteLine("Enter a Number");
-num = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter a Number");
+        num = int.Parse(Console.ReadLine());
 
-temp = num;
-power = GetDigitCount(num);
+        temp = num;
+        power = GetDigitCount(num);
 
-while (temp > 0){
-digit = temp % 10;
-result += (int)Math.Pow(digit, power);
-temp /= 10;
-}
+        while (temp > 0)
+        {
+            digit = temp % 10;
+            result += (int)Math.Pow(digit, power);
+            temp /= 10;
+        }
 
-if (result == num){ 
-Console.WriteLine("Armstrong");
-} 
+        if (result == num)
+        {
+            Console.WriteLine("Armstrong");
+        }
 
-else{
-Console.WriteLine("Not a Armstrong");
-}
+        else
+        {
+            Console.WriteLine("Not a Armstrong");
+        }
 
-}
+    }
 
-static int GetDigitCount(int n){
-int count = 0;
+    static int GetDigitCount(int n)
+    {
+        int count = 0;
 
-while(n>0){
-count++;
-n /= 10;
-}
-return count;
-}
+        while (n > 0)
+        {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
 
 }
