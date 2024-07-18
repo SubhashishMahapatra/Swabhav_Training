17665ed9b093b3ab4bf42194e7594a12e2ffa034
Subhashish Mahapatra <92688857+SubhashishMahapatra@users.noreply.github.com>
2024-07-18 09:57:14 +0530

    Create String_Manipulation.cs

---
+++ String_Manipulation.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Developer name is : " + args[0]);
+            Console.WriteLine("Company name is : " + args[1]);
+            Console.WriteLine("Locaiton is : "+args[2]);
+        }
+    }
+}
+
+
